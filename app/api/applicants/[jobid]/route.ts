177e5dd58b5db4c1ba8cf883c3478ad33482ed78
sanskrutikadam86-dev <sanskrutikadam86@gmail.com>
2026-07-29 12:27:19 +0530

    Fix params.jobid

--- app/api/applicants/[jobid]/route.ts
+++ app/api/applicants/[jobid]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import client from "@/lib/mongodb";
-import { Params } from "next/dist/server/request/params";
+
 
 export async function GET(
   req: NextRequest,
